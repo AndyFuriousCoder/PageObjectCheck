3d0541e10793e5723339bbce0bcdfe610cc90692
AndyFuriousCoder <andrew03.89@mail.ru>
2016-10-26 21:08:33 +0300

    Signed-off-by: AndyFuriousCoder <andrew03.89@mail.ru>

--- PageObjectChecking/src/main/java/pageObjects/Element.java
+++ PageObjectChecking/src/main/java/pageObjects/Element.java
@@ -1,6 +1,5 @@
 package pageObjects;
 
-import org.apache.http.util.Asserts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
--- PageObjectChecking/src/test/java/testCode/PageObjectChecking.java
+++ PageObjectChecking/src/test/java/testCode/PageObjectChecking.java
@@ -1,14 +1,8 @@
 package testCode;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.Logs;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
@@ -16,34 +10,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import pageObjects.CheckBoxElement;
 import pageObjects.LoginElement;
 import pageObjects.RadioButtonElement;
-
-import java.io.*;
 import java.util.concurrent.TimeUnit;
-
 import static org.testng.Assert.assertTrue;
 
 public class PageObjectChecking {
     private WebDriver driver; //used WebDriver
-  
 
     //Select browser
-    @Parameters("browser")
+    //@Parameters("browser")
+
 
     @BeforeTest(alwaysRun = true)
-    public void selectBrowser(String browser)
+    public void setUp()
     {
+        //String browser = java.lang.System.getProperties().getProperty("webbrowser");
+        String browser = "firefox";
         if (browser.equalsIgnoreCase("firefox")) driver = new FirefoxDriver();
         if (browser.equalsIgnoreCase("ie")) driver = new InternetExplorerDriver();
         if (browser.equalsIgnoreCase("chrome")) driver = new ChromeDriver();
-        if (browser.equalsIgnoreCase("opera")) driver = new OperaDriver();
+       // if (browser.equalsIgnoreCase("opera")) driver = new OperaDriver();
         if (browser.equalsIgnoreCase("safari")) driver = new SafariDriver();
-    }
-
-    @BeforeClass(alwaysRun = true)
-    public void setUp() {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
+
 
     //Login test
     @Test(enabled = true, dataProviderClass = LoginData.class, dataProvider = "LoginTest")
@@ -79,31 +69,10 @@ public class PageObjectChecking {
         new RadioButtonElement(driver, "Bronze").check();
         new RadioButtonElement(driver, "Selen").check();
 
-        //get screenshot
-        File screenshot = ((TakesScreenshot) driver).
-                getScreenshotAs(OutputType.FILE);
-        String path = "./src/test/java/logsAndScreenshots/screenshots/" + screenshot.getName();
-        try
-        {
-            FileUtils.copyFile(screenshot, new File(path));
-        } catch (Exception e) {}
     }
 
     @AfterTest(alwaysRun = true)
     public void tearDown() {
-        //Logs--------------------------------------------
-        Logs logs = driver.manage().logs();
-        LogEntries logEntries = logs.get(LogType.DRIVER);
-        try{
-        FileWriter writer = new FileWriter("./src/test/java/logsAndScreenshots/Logs.txt");
-            for (LogEntry logEntry : logEntries) {
-                writer.write(logEntry.getMessage() + "\n");
-            }
-            writer.flush();
-            writer.close();
-        } catch (Exception o) {System.out.println("File not found!");}
-        //-------------------------------------------------
-
         driver.close();
         driver.quit();
     }
